c80331491f2835332ead1b45e9c6a37e40705174
MariuszMJ <mariusz.michna.j@gmail.com>
2023-04-03 19:33:24 +0200

    get_model transfert to model.utils

--- train.py
+++ train.py
@@ -1,28 +1,17 @@
 import argparse
 import yaml
 import pandas as pd
-from models.linear_regression import LinearRegressor
-from models.model_interface import BaseModel
+from models.utils import get_model
 
 
 def main(config_path: str) -> None:
     with open(config_path) as file:
         config = yaml.safe_load(file)
-
     X_train = pd.read_csv(config['data']['train_data'])
     y_train = pd.read_csv(config['data']['train_target'])
-
     model = get_model(config['model']['name'])
     model.fit(X_train, y_train)
-
     model.save(config['model']['save_path'])
-
-
-def get_model(model_name: str) -> BaseModel:
-    if model_name == 'linear_regression':
-        return LinearRegressor()
-    else:
-        raise NotImplementedError(f"{model_name} is not implemented.")
 
 
 if __name__ == "__main__":
