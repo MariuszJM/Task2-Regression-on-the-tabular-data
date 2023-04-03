c3e6ab7c1a7ecce34603cedefac0609c0d719407
MariuszMJ <mariusz.michna.j@gmail.com>
2023-04-03 20:06:20 +0200

    log info to train has been aded

--- train.py
+++ train.py
@@ -1,18 +1,34 @@
 import argparse
 import yaml
 import pandas as pd
+import logging
+
 from models.utils import get_model
+
+# Set up logging configuration
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 def main(config_path: str) -> None:
+    logger.info(f"Loading configuration from {config_path}")
     with open(config_path) as file:
         config = yaml.safe_load(file)
+
+    logger.info("Loading training data")
     data = pd.read_csv(config['data']['train_data'])
     X_train = data.drop("target", axis=1)
     y_train = data["target"]
+
+    logger.info("Initializing the model")
     model = get_model(config['model']['name'])
+
+    logger.info("Training the model")
     model.fit(X_train, y_train)
+
+    logger.info("Saving the model")
     model.save(config['model']['save_path'])
+    logger.info("Model training completed")
 
 
 if __name__ == "__main__":
